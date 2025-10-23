76aef0c93b5a18fb96c29d351282e148d8d30c16
francescoshox <157975846+francescoshox@users.noreply.github.com>
2025-10-23 18:05:05 +0200

    GLU cleanup.

--- src/linax/architecture/channel_mixers/glu.py
+++ src/linax/architecture/channel_mixers/glu.py
@@ -6,30 +6,34 @@ See: copied from LinOSS implementation.
 import equinox as eqx
 import jax
 import jax.random as jr
+from jaxtyping import Array, PRNGKeyArray
 
 
 class GLU(eqx.Module):
-    """Gated Linear Unit (GLU) layer."""
+    """Gated Linear Unit (GLU) layer.
+
+    Args:
+        input_dim:
+          Dimensionality of the input features.
+        output_dim:
+          Dimensionality of the output features.
+        key:
+          JAX random key for initialization.
+
+    Source:
+        https://arxiv.org/pdf/2002.05202
+    """
 
     w1: eqx.nn.Linear
     w2: eqx.nn.Linear
 
-    def __init__(self, input_dim, output_dim, key):
-        """Initialize the GLU layer.
-
-        Args:
-            input_dim:
-              Dimensionality of the input features.
-            output_dim:
-              Dimensionality of the output features.
-            key:
-              JAX random key for initialization.
-        """
+    def __init__(self, input_dim: int, output_dim: int, key: PRNGKeyArray):
         w1_key, w2_key = jr.split(key, 2)
+
         self.w1 = eqx.nn.Linear(input_dim, output_dim, use_bias=True, key=w1_key)
         self.w2 = eqx.nn.Linear(input_dim, output_dim, use_bias=True, key=w2_key)
 
-    def __call__(self, x):
+    def __call__(self, x: Array) -> Array:
         """Forward pass of the GLU layer.
 
         Args:
