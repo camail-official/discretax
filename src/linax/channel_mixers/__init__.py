f6da940058414600b38a6ab6f6c851e0626544fa
pnazari <pnazari@student.ethz.ch>
2025-10-27 09:42:18 +0100

    ran ruff check

--- src/linax/channel_mixers/__init__.py
+++ src/linax/channel_mixers/__init__.py
@@ -5,5 +5,4 @@ from linax.channel_mixers.identity import IdentityChannelMixer
 from linax.channel_mixers.mlp import MLPChannelMixer
 from linax.channel_mixers.swi_glu import SwiGLU
 
-
 __all__ = ["GLU", "SwiGLU", "IdentityChannelMixer", "MLPChannelMixer"]
